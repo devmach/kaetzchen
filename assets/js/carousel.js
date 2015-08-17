dcc7ea036e73d039f115a79951c9e875aa9d5916
Aydın Ülfer <root@devmach.com>
2015-08-17 11:18:49 -0400

    Finish carousel

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,5 +1,50 @@
 var app = angular.module('app', ['ngAnimate']);
 
-app.controller('CarouselController', function($scope) {
-  $scope.msg = 'Hello there!';
-})
+app.controller('CarouselController', function($scope, $interval) {
+  $scope.images = [
+    './assets/img/kitten_1.jpg',
+    './assets/img/kitten_2.jpg',
+    './assets/img/kitten_3.jpg',
+  ];
+
+  $scope.activeImage = 0;
+  $scope.rotateImages = true;
+  $scope.shouldAutoRotate = true;
+
+  $scope.rotate = function() {
+    $scope.timer = $interval(function() {
+      $scope.nextSlide();
+    }, 2000);
+  };
+
+  $scope.startStopAutoRotate = function(val) {
+    $scope.rotateImages = val;
+
+    if (val) {
+      $scope.rotate();
+    } else {
+      $interval.cancel($scope.timer);
+    }
+  };
+
+  $scope.goToSlide = function(idx) {
+    $scope.activeImage = idx;
+    if ($scope.rotateImages) {
+      $scope.rotate();
+    }
+  };
+
+  $scope.nextSlide = function() {
+    $interval.cancel($scope.timer);
+    $scope.goToSlide(($scope.activeImage + 1) % $scope.images.length);
+  };
+
+  $scope.prevSlide = function() {
+    $interval.cancel($scope.timer);
+    $scope.goToSlide( $scope.activeImage === 0 ? ($scope.images.length-1) : $scope.activeImage - 1);
+  };
+
+  if ($scope.rotateImages) {
+    $scope.rotate();
+  }
+});
